f10d863aedab1a59e773dc337a8dc7f93098cbb4
bassima_gh <ghba20wu@student.ju.se>
2023-08-25 19:46:11 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import WorkExperience from "./components/WorkExperience";
 function App() {
   return (
     <>
-      <div className="overflow-hidden bg-primaryDark">
+      <div className="overflow-hidden bg-primaryDark m-1">
         <header>
           <Navigation />
         </header>
